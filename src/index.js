f772c4c76dea7ceca98b6db0afee1983546abd63
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-29 19:29:06 +0900

    default value

--- src/index.js
+++ src/index.js
@@ -8,25 +8,7 @@ document.getElementById("app").innerHTML = `
 `;
 
 /**
- * 分割代入
+ * デフォルト値、引数
  */
-
-// const myProfile = {
-//   name: "じゃけえ",
-//   age: 28,
-// };
-
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-// console.log(message1);
-
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message2);
-
-const myProfile = ["じゃけえ", 28];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
-console.log(message3);
-
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}です。`;
-console.log(message4);
+const sayHello = (name = "ゲスト") => console.log(`こんにちは! ${name}さん!`);
+sayHello("太郎");
