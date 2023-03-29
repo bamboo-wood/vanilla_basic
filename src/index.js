143945161ea191521fcd3a47372debc6142e3bf5
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-29 17:44:59 +0900

    template strings

--- src/index.js
+++ src/index.js
@@ -8,49 +8,15 @@ document.getElementById("app").innerHTML = `
 `;
 
 /**
- * const, let等の変数宣言
+ * テンプレート文字列
  */
-var val1 = "var変数";
-console.log(val1);
+const name = "Kauzki";
+const age = 28;
 
-// var変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+// old
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
 
-// var変数は再宣言可能
-var val1 = "var変数を再宣言";
-console.log(val1);
-
-// letは上書き可能
-let val2 = "let変数";
-console.log(val2);
-
-val2 = "let変数を上書き";
-console.log(val2);
-
-// letは再宣言不可能
-// let val2 = "let変数を再宣言";
-
-const val3 = "const変数";
-console.log(val3);
-
-// constは上書き不可能
-// val3 = "const変数を上書き";
-
-// const変数は再宣言不可能
-// const val3 = "const変数を再宣言";
-
-// constで定義したオブジェクトはプロパティの変更が可能
-const val4 = {
-  name: "じゃけえ",
-  age: 28,
-};
-val4.name = "jak";
-val4.address = "Hiroshima";
-console.log(val4);
-
-// constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// new
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
