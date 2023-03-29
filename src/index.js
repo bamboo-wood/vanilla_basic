fb5c39305a1219e2363e37d908a3bd5b34a4c43b
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-29 18:52:12 +0900

    destructuring assignment

--- src/index.js
+++ src/index.js
@@ -8,18 +8,25 @@ document.getElementById("app").innerHTML = `
 `;
 
 /**
- * アロー関数
+ * 分割代入
  */
 
-// old
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28,
+// };
 
-// new
-const func2 = (str) => str;
-console.log(func2("func2です"));
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけえ", 28];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
