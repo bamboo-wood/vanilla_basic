df9ade60b47a965b77b9757ededcf2d00f98f355
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-30 13:36:30 +0900

    todo app in vanilla

--- src/index.js
+++ src/index.js
@@ -1,44 +1,85 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
 
-/**
- * map, filter, reduce
- */
+  // テキストボックスが空の場合は処理を終了する
+  if (inputText === "") {
+    return;
+  }
 
-const nameArr = ["田中", "山田", "佐藤"];
+  createIncompleteList(inputText);
+};
 
-// for (let index = 0; index < nameArr.length; index++) {
-//   console.log(`${index + 1}番目は${nameArr[index]}`);
-// }
+// 未完了リストから指定の要素を削除する関数
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
 
-/**
- * map
- */
-// const nameArr2 = nameArr.map((name) => {
-//   return name;
-// });
-// console.log(nameArr2);
+// 未完了リストに指定の要素を追加する関数
+const createIncompleteList = (text) => {
+  document.getElementById("add-text").value = "";
 
-// nameArr.map((name) => console.log(name));
+  // divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
 
-/**
- * filter
- */
-// const numArr = [1, 2, 3, 4, 5];
-// const oddArr = numArr.filter((num) => num % 2 === 1);
-// console.log(oddArr);
+  // liタグを生成
+  const li = document.createElement("li");
+  li.innerText = text;
 
-/**
- * index
- */
-// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+  // button(完了)タグを生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
 
-const newNameArr = nameArr.map((name) =>
-  name === "佐藤" ? name : `${name}さん`
-);
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグを生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(div)を未完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  // button(削除)タグを生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
